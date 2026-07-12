b87e9f1f7280d78dec45eb5a29d0acd1cdec370a
kartikeyyy <kartikeykvk369@gmail.com>
2026-07-12 21:11:20 +0530

    Add program to calculate the length of a string

---
+++ Lengthofthestring.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string inputt;
+    
+    getline(cin, inputt);
+    
+    cout << inputt.length() << endl;
+    
+    return 0;
+}
